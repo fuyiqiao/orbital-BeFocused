0b58448acb0525089c47ab9f279b50ce61c1d05c
fuyiqiao <yiqiao_fu@hotmail.com>
2023-06-20 18:30:33 +0800

    added pause and quit functions, camera prompt, and linked to camera page

--- BeFocused/app/(home)/timer.jsx
+++ BeFocused/app/(home)/timer.jsx
@@ -1,14 +1,11 @@
-import { SafeAreaView, StyleSheet, Alert, FlatList, Pressable, View, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, Alert, FlatList, Pressable, View, Button, Modal } from 'react-native';
 import { supabase } from '../../lib/supabase';
 import { useEffect, useState } from 'react';
 import { Checkbox, Text } from 'react-native-paper';
 import { useRouter } from 'expo-router';
-import CountDown from 'react-native-countdown-component';
 import moment from 'moment';
 import {React} from 'react'; 
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { renderNode } from 'react-native-elements/dist/helpers';
-import ReactDOM from "react-dom";
+import { Link } from "expo-router";
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 import SelectDropdown from 'react-native-select-dropdown'
 
@@ -16,6 +13,7 @@ export default function CountDownTimer() {
   const [key, setKey] = useState(0);
   const [time, setTime] = useState(30); 
   const [playing, setPlaying] = useState(false); 
+  const [showModal, setShowModal] = useState(true);
   const durations = [10, 20, 30, 40, 50, 60];
   let set_time = 30; 
 
@@ -49,6 +47,39 @@ export default function CountDownTimer() {
     }
   }
 
+  const CameraPrompt = () => {
+    return(
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}>
+        <View
+          style={{
+            height: '35%',
+            marginTop: 'auto',
+            backgroundColor:'white'
+          }}>
+          <Text style={styles.title}>Break Time!</Text>
+          <Text style={styles.breakTimeText}>Take a break and update your study timeline! Open the camera app and show your progress</Text>
+          <View style={styles.dismissButton}>
+            <Link href="/camera">
+              <View style={styles.openCameraButton}>
+                <Text>Open Camera</Text>
+              </View>
+            </Link>
+          </View>
+          
+          <View>
+            <Button
+              title="Snooze with 1 Focus Coin"
+              onPress={() => setShowModal(false)}
+            />
+          </View>
+        </View>
+      </Modal>
+    ); 
+  }
+
   return(
     <View style = {styles.timerContainer}>
       <CountdownCircleTimer
@@ -60,14 +91,27 @@ export default function CountDownTimer() {
         onComplete={() => ({ shouldRepeat: false, delay: 1 })}
       >
         {({ remainingTime }) => 
-          {return (
-            <View>
-              <Text style={styles.timeDisplayContainer}>
-                {Math.floor(remainingTime / 60)} min
-              </Text>
-              <Text style={styles.remainingContainer}>remaining</Text>
-            </View>
-            );}}
+          { if (remainingTime == 590) {
+              setShowModal(true)
+            }
+            
+            if (remainingTime % 60 < 10) {
+            return (
+              <View>
+                <Text style={styles.timeDisplayContainer}>
+                  {Math.floor(remainingTime / 60)}:0{remainingTime % 60}
+                </Text>
+                <Text style={styles.remainingContainer}>remaining</Text>
+              </View> ); 
+          } else {
+            return (
+              <View>
+                <Text style={styles.timeDisplayContainer}>
+                  {Math.floor(remainingTime / 60)}:{remainingTime % 60}
+                </Text>
+                <Text style={styles.remainingContainer}>remaining</Text>
+              </View> ); 
+          }}}
       </CountdownCircleTimer>
       <PauseAndQuit />
       <SelectDropdown
@@ -87,6 +131,7 @@ export default function CountDownTimer() {
         }}
         buttonStyle={{height: 50, width: 300, borderWidth: 1, borderColor: '#304d6b'}}
       />
+      <CameraPrompt />
     </View>
   );
 }
@@ -103,7 +148,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
-    padding: 20,
+    padding: 15,
   },
   timerContainer: {
     display: "flex", 
@@ -126,4 +171,21 @@ const styles = StyleSheet.create({
     color: '#000000',
     textAlign: 'center',
   }, 
+  dismissButton: {
+    alignItems: 'center',
+    backgroundColor: '#304d6b',
+    borderRadius: 40,
+    color: '#FFFFFF', 
+    width: 300, 
+    justifyContent: 'center',
+  },
+  breakTimeText: {
+    textAlign: 'center',
+    fontSize: 13,
+    padding: 20,
+  }, 
+  openCameraButton: {
+    color: '#FFFFFF', 
+    height: 20, 
+  }
 });
